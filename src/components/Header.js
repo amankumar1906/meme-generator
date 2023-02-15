df96327d5879864cfc1537deb32635f1c8db8dba
Aman Kumar <amankumar7506@gmail.com>
2023-02-15 18:55:12 -0500

    new project commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img className="header--image" src="Troll face.png" />
+      <h2 className="header--title"> Meme Generator </h2>
+      <h4 className="header--project"> React Project</h4>
+    </header>
+  );
+}
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main>
+      <form className="form">
+        <input placeholder="Top text" className="form--input" type="text" />
+        <input placeholder="Bottom Text" className="form--input" type="text" />
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+}
